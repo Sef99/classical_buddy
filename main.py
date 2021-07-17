448e05854ad50fb5f1df6e8b4128afb5e983b2dd
Sef <enriquecaballero01@hotmail.com>
2021-07-17 19:40:19 -0400

    Fixed some bugs

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
 
 bot = commands.Bot(command_prefix='!')
 
-async def process_composer(ctx, composer_details, wikipedia_desc):
+async def process_composer(ctx, composer_details):
     if await composer_existence(ctx, composer_details):
         for composers in composer_details['composers']:
+                wikipedia_desc = wikiapi(composers["complete_name"])
                 sep = '\n'
                 pageid = wikipedia_desc['query']['pageids'][0]
                 stripped = wikipedia_desc['query']['pages'][str(pageid)]['extract'].split(sep, 1)[0]
@@ -48,21 +49,19 @@ async def _help(ctx):
     embed_message.add_field(name='!desc composer', value='Returns a brief description of the composer.', inline=False)
     embed_message.add_field(name='!random', value='Returns a brief description of a random composer.', inline=False)
     embed_message.add_field(name='!works composer', value='Returns a list of recommended works of the composer.', inline=False)
-    embed_message.add_field(name='!rec composer', value='Recommends and plays a random piece from the given composer.', inline=False)
     embed_message.add_field(name='!rec composer', value='Recommends and plays a random piece from the given composer. \nTo pause and resume: !pause and !resume.\nTo make the bot leave the channel: !leave', inline=False)
     await ctx.send(embed=embed_message)
 
 @bot.command(name='desc')
 async def _search_name(ctx, composer_name):
     composer_details = get_composerdet(composer_name)
-    wikipedia_desc = wikiapi(composer_name)
-    await process_composer(ctx, composer_details, wikipedia_desc)
+    await process_composer(ctx, composer_details)
 
 @bot.command(name='random')
 async def _search_random(ctx):
     composer_details = get_random()
     wikipedia_desc = wikiapi(composer_details['composers'][0]['complete_name'])
-    await process_composer(ctx, composer_details, wikipedia_desc)
+    await process_composer(ctx, composer_details)
 
 @bot.command(name='works')
 async def _search_works(ctx, composer_name):
@@ -97,14 +96,20 @@ async def _rec_works(ctx, composer_name):
 #needs only the video id of a youtube video, rather than the full url, in order to work. i.e. !p DZTAXk2NOdc
 @bot.command(name='p')
 async def _play_video(ctx, video_id):
-    vc = ctx.author.voice.channel
-    await vc.connect()
+    bot_vc = ctx.voice_client
+    if not bot_vc or not bot_vc.is_connected():
+        vc = ctx.author.voice.channel
+        await vc.connect()
     voice = ctx.voice_client
     url = 'https://www.youtube.com/watch?v=' + video_id
     info = YoutubeDL(YDL_OPTIONS).extract_info(url, download=False)
     iurl = info['formats'][0]['url']
     source = await discord.FFmpegOpusAudio.from_probe(iurl, **FFMPEG_OPTIONS)
-    voice.play(source)
+    if not voice.is_playing():
+        voice.play(source)
+    else:
+        voice.stop()
+        voice.play(source)
 
 @bot.command(name='pause')
 async def _pause(ctx):
